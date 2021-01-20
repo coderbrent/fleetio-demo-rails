40197d3ced84c804ab94a95e2c38824283be560f
Brent Abruzese <brent.abruzese@gmail.com>
2021-01-20 12:08:47 -0500

    added postpurge

--- postcss.config.js
+++ postcss.config.js
@@ -9,5 +9,21 @@ module.exports = {
       },
       stage: 3
     }),
-  ], 
+  ]
 }
+
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.vue',
+        './app/javascript/**/*.jsx',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
+}
+  
